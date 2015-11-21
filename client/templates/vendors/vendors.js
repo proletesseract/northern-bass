a5def748e1ccb278a6037fcdcf95c8375ad32d77
Paul Grey <work@paulgrey.co.nz>
2015-11-21 14:49:24 +1300

    vendors.js

---
+++ client/templates/vendors/vendors.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+
+  Template.vendors.helpers({
+    vendors: function(){
+        return Vendors.find({});
+    }
+  });
+}

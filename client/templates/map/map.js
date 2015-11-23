e612c1d845d18fda3f1752c528a416c0b59b8951
Paul Grey <work@paulgrey.co.nz>
2015-11-23 20:19:08 +1300

    google maps

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,6 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
 if (Meteor.isClient) {
-	Template.map.helpers({
+  // This code only runs on the client
 
+  Template.routeMap.helpers({
 
-	});
+  });
+}
+
+Template.routeMap.onRendered(function(){
+	initMap();
+})
+
+initMap = function(){
+    var map;
+      var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644)
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+  mapOptions);
 }

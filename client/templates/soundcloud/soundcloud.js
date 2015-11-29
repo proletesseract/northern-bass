88d5f7c0d96a9351a104e17e044800f6daeb160b
Paul Grey <work@paulgrey.co.nz>
2015-11-29 18:07:54 +1300

    soundcloud test

--- client/templates/soundcloud/soundcloud.js
+++ client/templates/soundcloud/soundcloud.js
@@ -15,7 +15,7 @@ initSoundCloud = function() {
 	  client_id: '40bc6ae123807c628cc971116092a93d'
 	});
 
-	var track_url = 'https://soundcloud.com/skeptical/skeptical-blue-eyes-vip';
+	var track_url = 'http://soundcloud.com/paul-g-nz/sets/northern-bass-15-16';
 	SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
 	  console.log('oEmbed response: ', oEmbed);
 	  

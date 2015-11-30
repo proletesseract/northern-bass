33d56b6662446673447c2938c022679953f9d5fb
Chris Grimshaw-Jones <chrisgrimshawjones@gmail.com>
2015-11-30 22:09:07 +1300

    added geolocation view

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,94 +1,79 @@
-/* 
+/**
+ * Created by chrisgrimshaw-jones on 27/11/15.
+ */
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    loc: function () {
-      // return 0, 0 if the location isn't ready
-      return Geolocation.latLng() || { lat: 0, lng: 0 };
-    },
-    error: Geolocation.error
-  });
-  
-  Template.routeMap.helpers({
+    // This code only runs on the client
+    Template.body.helpers({
+        loc: function () {
+            // return 0, 0 if the location isn't ready
+            return Geolocation.latLng() || { lat: 0, lng: 0 };
+        },
+        error: Geolocation.error
+    });
 
-  });
+    Template.routeMap.helpers({
+
+    });
 }
 
 Template.routeMap.onRendered(function(){
-	initMap();
+    initMap();
 })
 
 
-/*initMap = function(){
-    var map;
-      var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'),
-  mapOptions);
-}
-*/
-/*
-initMap = function(){
-
-
-    var auckland = {lat: 50, lng: 50};
-
-    var map = $("#map-canvas");
-      var mapOptions = {
-        zoom: 22,
-        center: new google.maps.LatLng(-36.1620727, 174.4867605),
-        styles: [{
-          stylers: [
-            { lightness: 0 },
-            { saturation: 10 },
-            { hue: "#5E419B"}
-          ]
-        }]
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-}*/
-
 initMap = function() {
-  var auckland = {lat: -36.848448, lng: 174.7600023};
-  var settlementRd = {lat: -36.1618735, lng: 174.4867605};
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+  } else {
+      alert("There is Some Problem on your current browser to get Geo Location!");
+  }
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: settlementRd,
-    scrollwheel: false,
-    zoomControl: true,    
-    zoom: 12,
-    styles: [{
-      stylers: [
-      { lightness: 0 },
-      { saturation: 10 },
-      { hue: "#5E419B" }]
-    }]
-  });
+  function success(position) {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      var city = position.coords.locality;
+      var LatLng = new google.maps.LatLng(lat, long);
+      var mapOptions = {
+          center: LatLng,
+          zoom: 8,
+          styles: [{
+            stylers: [
+            { lightness: 5 },
+            { saturation: 0 },
+            { hue: "#5E419B" }]
+          }],
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map
-  });
+      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  // Set destination, origin and travel mode.
-  var request = {
-    destination: settlementRd,
-    origin: auckland,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
+      var settlementRd = {lat: -36.1618735, lng: 174.4867605};
 
-  // Pass the directions request to the directions service.
-  var directionsService = new google.maps.DirectionsService();
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      // Display the route on the map.
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
+      var _image = new google.maps.MarkerImage('images/map/nb-marker.png',  // size 28 x 42
+          null,
+          new google.maps.Point(0, 0), // The origin for this image is 0,0.
+          new google.maps.Point(12, 35) // The anchor for this image is the base of the flagpole at 60,20.
+      );
+
+      // Set Marker
+      var marker = new google.maps.Marker({
+          position: settlementRd,
+          icon: _image,
+          map: map,
+          title: 'Northern Bass',
+          animation: google.maps.Animation.DROP,
+      });
+
+      marker.addListener('click', toggleBounce);
+
+      function toggleBounce() {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+
+  }  // end success - got the current position
+} // end initMap()
